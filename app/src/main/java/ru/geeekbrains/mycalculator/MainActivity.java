fc491201775ef2368d4012c5e9daac264e80944e
PrinceSch <sofy95@mail.ru>
2021-06-07 22:43:36 +0700

    2. Доделайте приложение «Калькулятор». Это последний урок с созданием приложения «Калькулятор». (конкретно - доделала Parcelable, остальное все вроде было выполнено)

--- app/src/main/java/ru/geeekbrains/mycalculator/MainActivity.java
+++ app/src/main/java/ru/geeekbrains/mycalculator/MainActivity.java
@@ -1,11 +1,10 @@
-package ru.geeekbrains.mycalculator;
+ package ru.geeekbrains.mycalculator;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -30,13 +29,10 @@ public class MainActivity extends AppCompatActivity {
         textViewSecond = findViewById(R.id.text_view_second);
 
         Button buttonSettings = findViewById(R.id.button_settings);
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(runSettings);
-            //как пересоздавать активити из другого? новый запуск? мой текущий вариант рабочий, но чую, что это костыль.
-            }
+        buttonSettings.setOnClickListener(v -> {
+            Intent runSettings = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(runSettings);
+        //как пересоздавать активити из другого? новый запуск? мой текущий вариант рабочий, но чую, что это костыль.
         });
 
         Button button0 = findViewById(R.id.button_0);
@@ -110,6 +106,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         calculatorLogic = savedInstanceState.getParcelable(keyNumbers);
-        //не могу сообразить, что дальше жедать с этим parcelable - данные сохраняются при повороте экрана, но не отображаются.
+        setField(textViewFirst, String.valueOf(calculatorLogic.getNumberOne()));
+        setField(textViewSecond, String.valueOf(calculatorLogic.getNumberTwo()));
+        setField(textViewOperation, calculatorLogic.getOperator());
+    }
+
+    private void setField(TextView field, String value){
+        field.setText(value);
     }
 }
